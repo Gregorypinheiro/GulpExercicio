b459eb2dbf0023291230f46bdc73355c1eb0ad9c
gregory <gregorypinheiro88@gmail.com>
2024-08-23 11:06:19 -0300

    mimificação js ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const unglify = require('gulp-uglify');
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+    .pipe(unglify())
+    .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -16,3 +23,4 @@ exports.sass = compilaSass;
 exports.watch = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false}, gulp.series(compilaSass));
 }
+exports.javascript = comprimeJavaScript;
