8c04d6d2676afda9a71bb929035c44fc89d149cd
gregory <gregorypinheiro88@gmail.com>
2024-08-23 10:41:34 -0300

    compilaSass

--- gulpfile.js
+++ gulpfile.js
@@ -12,23 +12,6 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-function funcaoPadrao(callback) {
-    console.log("Executando via Gulp");
-    callback();
-}
-
-function dizOi(callback){
-    console.log("Olá Gulp");
-    dizTchau();
-    callback();
-}
-
-function dizTchau() {
-    console.log("Tchau Gulp");
-}
-
-exports.default = gulp.parallel(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
 exports.sass = compilaSass;
 exports.watch = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false}, gulp.series(compilaSass));
